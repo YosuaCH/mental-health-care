d65f5c4ec4ae89b6ceb852c41231d37556117b83
YosuaCH <yosuach38@gmail.com>
2026-02-17 06:03:56 +0700

    connecting signUp mitra  page with be

---
+++ assets/js/pages/signUpMitra.js
@@ -0,0 +1,39 @@
+import { registerPsikiater } from "../services/authService.js";
+
+const registerForm = document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const namaLengkap = document.getElementById("namaLengkap").value;
+  const noStr = document.getElementById("noStr").value;
+  const noWa = document.getElementById("noWa").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (password !== confirmPassword) {
+    alert("Password tidak sama!");
+    return;
+  }
+
+  try {
+    const res = await registerPsikiater({
+      email,
+      namaLengkap,
+      noStr,
+      noWa,
+      password,
+    });
+    const data = await res.json();
+
+    if (res.ok) {
+      alert(data.message || "Register sukses");
+      window.location.href = "login.html";
+    } else {
+      alert(data.message || "Register gagal");
+    }
+  } catch (err) {
+    alert("Server tidak bisa diakses");
+  }
+});
